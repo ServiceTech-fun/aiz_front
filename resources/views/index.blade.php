74b49adeada0eef21bda2db4e0b7f8e8d254e41e
立石凌 <tateishi@tateishiryounoMacBook-Pro.local>
2022-06-17 16:54:36 +0900

    add tailwindcss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>LAND's</title>
 </head>
+<header>
+    @include('section.header')
+</header>
 <body>
     <p>トップページ</p>
     <ul>
